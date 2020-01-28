77b782b6d3c87edacd355fa12fd7e15fba1d6691
Danilo Woznica <danilowoz@gmail.com>
2020-01-28 17:46:03 +0000

    fix(eslint): defines specifics rules and parser by filetype

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -3,13 +3,10 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
     "plugin:prettier/recommended"
   ],
   plugins: [
     "react",
-    "@typescript-eslint",
     "prettier",
     "react-hooks",
     "simple-import-sort",
@@ -29,12 +26,6 @@ module.exports = {
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
-    "@typescript-eslint/explicit-function-return-type": ["off"],
-    "@typescript-eslint/no-inferrable-types": 1,
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
-    ],
     "sort-exports/sort-exports": ["error", { sortDir: "asc" }],
     "simple-import-sort/sort": [
       "error",
@@ -50,9 +41,21 @@ module.exports = {
   },
   overrides: [
     {
-      files: ["**/*.tsx"],
+      files: ["**/*.ts?(x)"],
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint"
+      ],
+      plugins: ["@typescript-eslint"],
       rules: {
-        "react/prop-types": "off"
+        "react/prop-types": "off",
+        "@typescript-eslint/explicit-function-return-type": ["off"],
+        "@typescript-eslint/no-inferrable-types": 1,
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+        ]
       }
     }
   ]
